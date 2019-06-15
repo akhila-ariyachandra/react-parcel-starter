58063735a704504b0654d00676637333a1691fb1
akhila-ariyachandra <akhila_ariyachandra@live.com>
2019-06-15 14:29:21 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# react-parcel-starter
+# React Parcel Starter
+
+[Blog Post](https://akhila-ariyachandra.now.sh/post/61076cf3-d088-45d7-bad7-d4a6a9bc5387)
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1>React Parcel Starter</h1>
+    </div>
+  );
+};
+
+export default App;
