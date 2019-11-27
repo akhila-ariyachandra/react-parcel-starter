4fa9da4fdb6cc754ec03f7aa0d427e2f4dab54ea
akhila-ariyachandra <akhila_ariyachandra@live.com>
2019-11-27 21:59:10 +0530

    Remove core-js and add bundler visualizer

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App.js";
-import "core-js/stable";
 import "regenerator-runtime/runtime";
 
 ReactDOM.render(<App />, document.getElementById("root"));
