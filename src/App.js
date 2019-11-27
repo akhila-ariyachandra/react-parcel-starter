839267d1c6126038c4812325467fef3a51ddb8c0
akhila-ariyachandra <akhila_ariyachandra@live.com>
2019-11-27 21:50:24 +0530

    Updated with react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+// Import pages
+import Home from "./pages/home";
+import About from "./pages/about";
 
 const App = () => {
   return (
-    <div>
-      <h1>React Parcel Starter</h1>
-    </div>
+    <Router>
+      {/* A <Switch> looks through its children <Route>s and
+          renders the first one that matches the current URL. */}
+      <Switch>
+        <Route path="/about">
+          <About />
+        </Route>
+
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 };
 
