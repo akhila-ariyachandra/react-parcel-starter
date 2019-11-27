3dd21effddd259d14d7e031dfd1848dc410c4709
akhila-ariyachandra <akhila_ariyachandra@live.com>
2019-11-27 23:17:44 +0530

    Clean up

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 // Import pages
 import Home from "./pages/home";
